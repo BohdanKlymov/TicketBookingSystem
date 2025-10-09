fafc450e774f9bb0b72c66405fc8b0d9dd230b40
Bohdan Klymov <programmer.b.k.339@gmail.com>
2025-10-09 18:34:46 +0200

    Add files via upload
    Added a new <div> element with the ID list in the HTML file to display dynamic movie data.
    
    Converted the fetched movie data into JSON, then serialized it into a formatted string and displayed it inside the list container by dynamically creating and appending HTML elements.
    
    Added functionality to display the first movie image on the page using the image path stored in the local directory.

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,18 @@
 fetch("http://localhost:5000/get-all")
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => {
+    console.log(json);
+
+
+  const child = document.createElement("p");
+  child.innerHTML = JSON.stringify(json, null, 2);
+
+  const divList = document.getElementById("list");
+  divList.appendChild(child)
+
+  const picturesLink = "C:\AllMyProgramming\WebCinema\moviePictures";
+
+  var img = document.createElement("img");
+  img.src = picturesLink + "\download (2).jpg"
+  divList.appendChild(img)
+})
