2d3dd367f74fe8d4b721bd638672e8f57f11acdd
Bohdan Klymov <programmer.b.k.339@gmail.com>
2025-10-13 14:25:52 +0200

    Add files via upload
    Added list boxes displaying available movies on the page.
    
    Implemented the first onClick functionality.
    
    Each movie item in the list now also displays its ticket cost.

--- app/static/script.js
+++ app/static/script.js
@@ -11,10 +11,22 @@ fetch("http://localhost:5000/get-all")
       movieBlock.classList.add("movie-block");
 
       movieBlock.innerHTML = `
-        <h3>${movie.title}</h3>
-        <p>${movie.movie_description}</p>
+        <button id="movie">
+          <h3>${movie.title}</h3>
+          <p>${movie.movie_description}</p>
+          <p>${movie.price}$</p>
+        </button>
       `;
 
       divList.appendChild(movieBlock);
+
+      document.getElementById("movie").addEventListener("click", clickOnMovie);
+
+      
+      function clickOnMovie() {
+        document.getElementById("maintance-con").innerHTML = movie.title;
+      }
+
+
     });
   })
